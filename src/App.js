6cd2484f7f1451d24db7f16e82f4117dcd1b15c9
Mr-Vishal-Ranjan <vr842002@gmail.com>
2024-02-16 22:56:40 +0530

    1.0.0

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Time from './components/Time';
 class App extends Component {
   render() {
     return (
-      <div className='mainDiv'>
+      
+        <div className='mainDiv'>
         <Time/>
-      </div>
+        </div>
+      
     );
   }
 }
